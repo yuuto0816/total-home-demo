ad55cd0f9400418edd3275513e2ccb7aef94d90a
yuuto0816 <ktc24a31g0019@edu.kyoto-tech.ac.jp>
2026-04-10 15:22:16 +0900

    feat: add index.html and configure redirects in next.config.ts; remove unused Portal component

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/index.html",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
